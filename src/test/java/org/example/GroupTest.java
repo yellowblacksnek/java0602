e862339cb5420e13928ed0e88f49b89360f91435
sus <truemarat@gmail.com>
2023-02-13 19:18:08 +0300

    add tests

---
+++ src/test/java/org/example/GroupTest.java
@@ -0,0 +1,85 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GroupTest {
+
+    @Test
+    public void TestEndGroup() {
+        ArrayList<Map<String, Group>> columnMapList = new ArrayList<>();
+        Group groupA = new Group(columnMapList);
+        Group groupB = new Group(columnMapList);
+
+        groupA.setSuccessor(groupB);
+        assertEquals(groupB, groupA.getEndGroup());
+    }
+
+    @Test
+    public void TestEndGroupMultipleSuccessors() {
+        ArrayList<Map<String, Group>> columnMapList = new ArrayList<>();
+        ArrayList<Group> groups = new ArrayList<>();
+        int length = 10;
+        for(int i = 0; i < length; i++) {
+            groups.add(new Group(columnMapList));
+        }
+        for(int i = 0; i < length - 1; i++) {
+            groups.get(i).setSuccessor(groups.get(i+1));
+        }
+        assertEquals(groups.get(length-1), groups.get(0).getEndGroup());
+    }
+
+    @Test
+    public void TestAdd() {
+        ArrayList<Map<String, Group>> columnMapList = new ArrayList<>();
+        Group group = new Group(columnMapList);
+        String[] line = new String[]{"111", "222", "333"};
+
+        for(int i = 0; i < line.length; i++) {
+            columnMapList.add(new HashMap<>());
+        }
+        group.add(line);
+
+        for(int i = 0; i < line.length; i++) {
+            assertTrue(columnMapList.get(i).containsKey(line[i]));
+            assertEquals(group, columnMapList.get(i).get(line[i]));
+        }
+    }
+
+    @Test
+    public void TestAddSuccession() {
+        ArrayList<Map<String, Group>> columnMapList = new ArrayList<>();
+        Group group = new Group(columnMapList);
+        String[] line = new String[]{"111", "222", "333"};
+
+        for(int i = 0; i < line.length; i++) {
+            columnMapList.add(new HashMap<>());
+        }
+        group.add(line);
+
+        Group newGroup = new Group(columnMapList);
+        String[] newLine = new String[]{"0", "222", "5"};
+
+        newGroup.add(newLine);
+
+        for(int i = 0; i < line.length; i++) {
+            assertTrue(columnMapList.get(i).containsKey(line[i]));
+        }
+        for(int i = 0; i < newLine.length; i++) {
+            assertTrue(columnMapList.get(i).containsKey(newLine[i]));
+        }
+
+        columnMapList.forEach(map -> {
+            map.forEach((elemKey, groupValue) -> {
+                assertEquals(newGroup, groupValue.getEndGroup());
+            });
+        });
+    }
+}
---
+++ src/test/java/org/example/TaskTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class TaskTest {
+    private static Stream<Arguments> filesToGroups() {
+        return Stream.of(
+                arguments("test1.txt", 1),
+                arguments("test2.txt", 2),
+                arguments("test3.txt", 3)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("filesToGroups")
+    public void TestTask(String filePath, int groupsNum) throws IOException {
+        Path path = Path.of("src","test","resources", filePath);
+        Task task;
+        try(BufferedReader reader = Files.newBufferedReader(path)) {
+            Set<String> lines = reader.lines().collect(Collectors.toSet());
+            task = new Task(lines);
+        }
+        task.processLines();
+        assertEquals(groupsNum, task.getEndGroups().size());
+    }
+}
